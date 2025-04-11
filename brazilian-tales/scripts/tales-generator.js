7f416e6d874f9f46e8f5b90397f4e69b111d40b0
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-04-11 20:56:21 -0300

    fix: fixed bug in tales.generator

--- brazilian-tales/scripts/tales-generator.js
+++ brazilian-tales/scripts/tales-generator.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch('data/tales.json');
     const tales = await response.json();
 
-    function generateTaleCards() {
-        const talesGrid = document.getElementById("tales-grid");
-        tales.forEach(tale => {
-            const card = document.createElement("div");
-            card.className = "tale-card";
-    
-            card.innerHTML = `
+    const talesGrid = document.getElementById("tales-grid");
+    tales.forEach(tale => {
+        const card = document.createElement("div");
+        card.className = "tale-card";
+
+        card.innerHTML = `
                 <div class="tale-image">
                     <img src="${tale.image}" alt="${tale.name}">
                 </div>
@@ -18,11 +17,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                     <button class="favorite-btn" data-tale="${tale.name}">❤️ Add to Favorites</button>
                 </div>
             `;
-    
-            talesGrid.appendChild(card);
-        });
-    
-        document.dispatchEvent(new Event("talesGenerated"));
-    }
+
+        talesGrid.appendChild(card);
+    });
+
+    document.dispatchEvent(new Event("talesGenerated"));
 
 })
