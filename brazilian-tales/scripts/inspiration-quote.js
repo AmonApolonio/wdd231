d16190ee2286c43c707add5ba96cbcde9c8b3198
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-04-11 21:10:20 -0300

    feat: testing new API

--- brazilian-tales/scripts/inspiration-quote.js
+++ brazilian-tales/scripts/inspiration-quote.js
@@ -1,11 +1,15 @@
 async function fetchInspirationQuote() {
     try {
-        const response = await fetch('https://zenquotes.io/api/random');
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+            headers: {
+                'X-Api-Key': 'Z9KYimlo1OqLt3EIksk5Tg==IYVxvD5BJFTkePhq'
+            }
+        });
         const data = await response.json();
         const quote = data[0];
 
-        document.getElementById('quote-text').textContent = `"${quote.q}"`;
-        document.getElementById('quote-author').textContent = `- ${quote.a}`;
+        document.getElementById('quote-text').textContent = `"${quote.quote}"`;
+        document.getElementById('quote-author').textContent = `- ${quote.author}`;
     } catch (error) {
         console.error('Error fetching the quote:', error);
         document.getElementById('quote-text').textContent = 'Failed to load quote. Please try again later.';
